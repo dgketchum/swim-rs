ecd52c3adadb19088471cbc54e255eb323bd7969
dgketchum <dgketchum@gmail.com>
2024-02-05 12:23:55 -0700

    add awc and ksat soils data to field properties json write

--- prep/field_properties.py
+++ prep/field_properties.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 import pandas as pd
 
 
-def write_field_properties(shp, irr, cdl, soils, js):
+def write_field_properties(shp, irr, cdl, ssurgo, js):
     irr = pd.read_csv(irr, index_col='FID')
     irr.drop(columns=['LAT', 'LON'], inplace=True)
     dct = irr.T.to_dict()
@@ -19,19 +19,22 @@ def write_field_properties(shp, irr, cdl, soils, js):
     cdl = cdl.T.to_dict()
     [dct[k].update({'cdl': {int(kk.split('_')[1]): int(vv) for kk, vv in cdl[k].items()}}) for k in dct.keys()]
 
+    soils = pd.read_csv(ssurgo, index_col='FID')
+
+    awc = soils[['awc']].copy()
+    awc = awc.T.to_dict()
+    [dct[k].update({'awc': awc[k]['awc']}) for k in dct.keys()]
+
+    ksat = soils[['ksat']].copy()
+    ksat = ksat.T.to_dict()
+    [dct[k].update({'ksat': ksat[k]['ksat']}) for k in dct.keys()]
+
     fields = gpd.read_file(shp)
     fields.index = fields['FID']
-    awc = gpd.read_file(os.path.join(soils, 'AWC_WTA_0to152cm_statsgo.shp'), mask=fields)
-
-    intersect = gpd.overlay(awc, fields, how='intersection')
-    intersect['area'] = [g.area for g in intersect['geometry']]
-
-    for fid, row in fields.iterrows():
-        inter = intersect[intersect['FID'] == fid]
-        tot_area = inter['area'].sum()
-        awc = (inter['AWC'] * inter['area'] / tot_area).sum()
-
-        dct[fid]['stn_whc'] = awc * 12
+    fields['area_sq_m'] = [g.area for g in fields['geometry']]
+    area_sq_m = fields[['area_sq_m']].copy()
+    area_sq_m = area_sq_m.T.to_dict()
+    [dct[k].update({'area_sq_m': area_sq_m[k]['area_sq_m']}) for k in dct.keys()]
 
     with open(js, 'w') as fp:
         json.dump(dct, fp, indent=4)
@@ -41,8 +44,6 @@ if __name__ == '__main__':
 
     d = '/media/research/IrrigationGIS/swim'
 
-    soils_ = os.path.join(d, 'soils_aea')
-
     project = 'tongue'
     project_ws = os.path.join(d, 'examples', project)
 
@@ -50,8 +51,9 @@ if __name__ == '__main__':
 
     irr_ = os.path.join(project_ws, 'properties', '{}_irr.csv'.format(project))
     cdl_ = os.path.join(project_ws, 'properties', '{}_cdl.csv'.format(project))
+    _ssurgo = os.path.join(project_ws, 'properties', '{}_ssurgo.csv'.format(project))
     jsn = os.path.join(project_ws, 'properties', '{}_props.json'.format(project))
 
-    write_field_properties(fields_shp, irr_, cdl_, soils_, jsn)
+    write_field_properties(fields_shp, irr_, cdl_, _ssurgo, jsn)
 
 # ========================= EOF ====================================================================
