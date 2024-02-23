4ba5910108d82a3e31b14f0349dea85951b62a12
dgketchum <dgketchum@gmail.com>
2024-02-23 13:47:08 -0700

    only check results if debugging, else just write the results to .np

--- run/run_field_etd_mp.py
+++ run/run_field_etd_mp.py
@@ -10,7 +10,6 @@ from swim.input import SamplePlots
 
 
 def optimize_fields(ini_path, debug_flag=False, field_type='irrigated', project='tongue'):
-
     start_time = time.time()
 
     config = ProjectConfig(field_type=field_type)
@@ -20,31 +19,40 @@ def optimize_fields(ini_path, debug_flag=False, field_type='irrigated', project=
     fields.initialize_plot_data(config)
 
     df = obs_field_cycle.field_day_loop(config, fields, debug_flag=debug_flag)
-    pred = df['et_act'].values
 
-    np.savetxt(os.path.join(d, 'pest', 'eta.np'), pred)
+    # debug_flag=False just returns the ndarray for writing
+    if not debug_flag:
+        for i, fid in enumerate(fields.input['order']):
+            pred = df[:, 0, i]
+            np.savetxt(os.path.join(d, 'pest', 'obs', 'obs_eta_{}.np'.format(fid)), pred)
+            end_time = time.time()
+        print('Execution time: {:.2f} seconds'.format(end_time - start_time))
 
-    obs = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}/obs/obs_eta_1779.np'.format(project)
-    obs = np.loadtxt(obs)
-    cols = ['et_obs'] + list(df.columns)
-    df['et_obs'] = obs
-    df = df[cols]
-    a = df.loc['2019-01-01': '2021-01-01']
+    # debug returns a dataframe
+    if debug_flag:
+        pred = df['et_act'].values
 
-    comp = pd.DataFrame(data=np.vstack([obs, pred]).T, columns=['obs', 'pred'], index=df.index)
-    comp['eq'] = comp['obs'] == comp['pred']
+        obs = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}/obs/obs_eta_1779.np'.format(project)
+        obs = np.loadtxt(obs)
+        cols = ['et_obs'] + list(df.columns)
+        df['et_obs'] = obs
+        df = df[cols]
+        a = df.loc['2010-01-01': '2021-01-01']
 
-    rmse = np.sqrt(((pred - obs) ** 2).mean())
-    end_time = time.time()
-    print('Execution time: {:.2f} seconds'.format(end_time - start_time))
-    print('Mean Obs: {:.2f}, Mean Pred: {:.2f}'.format(obs.mean(), pred.mean()))
-    print('RMS Diff: {:.4f}\n\n\n\n'.format(rmse))
+        comp = pd.DataFrame(data=np.vstack([obs, pred]).T, columns=['obs', 'pred'], index=df.index)
+        comp['eq'] = comp['obs'] == comp['pred']
+        comp['capture'] = df['capture']
 
-    comp = comp.loc[a[a['capture'] == 1.0].index]
-    pred, obs = comp['pred'], comp['obs']
-    rmse = np.sqrt(((pred - obs) ** 2).mean())
-    print('RMSE Capture Dates: {:.4f}\n\n\n\n'.format(rmse))
-    pass
+        rmse = np.sqrt(((pred - obs) ** 2).mean())
+        end_time = time.time()
+        print('Execution time: {:.2f} seconds'.format(end_time - start_time))
+        print('Mean Obs: {:.2f}, Mean Pred: {:.2f}'.format(obs.mean(), pred.mean()))
+        print('RMS Diff: {:.4f}\n\n\n\n'.format(rmse))
+
+        comp = comp.loc[a[a['capture'] == 1.0].index]
+        pred, obs = comp['pred'], comp['obs']
+        rmse = np.sqrt(((pred - obs) ** 2).mean())
+        print('RMSE Capture Dates: {:.4f}\n\n\n\n'.format(rmse))
 
 
 if __name__ == '__main__':
