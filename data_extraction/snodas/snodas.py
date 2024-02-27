5e2badbc604ecddc25a91134220aee36eeae1d29
dgketchum <dgketchum@gmail.com>
2024-02-27 09:20:47 -0700

    sort and match index on snodas extraction

--- data_extraction/snodas/snodas.py
+++ data_extraction/snodas/snodas.py
@@ -8,13 +8,14 @@ from rasterstats import zonal_stats
 
 def snodas_zonal_stats(in_shp, raster_dir, out_js, targets=None):
     df = gpd.read_file(in_shp)
+    df.index = [i for i in df['FID']]
 
     if targets:
         df = df.loc[targets]
 
     geo, fids = list(df['geometry']), list(df['FID'])
 
-    l = [os.path.join(raster_dir, x) for x in os.listdir(raster_dir) if x.endswith('.tif')]
+    l = sorted([os.path.join(raster_dir, x) for x in os.listdir(raster_dir) if x.endswith('.tif')])
 
     dct = {}
 
@@ -31,6 +32,8 @@ def snodas_zonal_stats(in_shp, raster_dir, out_js, targets=None):
 
     with open(out_js, 'w') as fp:
         json.dump(dct, fp, indent=4)
+
+    print('wrote', out_js)
 
 
 if __name__ == '__main__':
