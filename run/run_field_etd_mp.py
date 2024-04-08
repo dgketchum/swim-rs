e262b1d3f50a91b80786c20ad79aa6bbdf8e7f54
dgketchum <dgketchum@gmail.com>
2024-04-08 17:36:35 -0600

    check rmse of swe prediction

--- run/run_field_etd_mp.py
+++ run/run_field_etd_mp.py
@@ -22,9 +22,11 @@ def optimize_fields(ini_path, debug_flag=False, field_type='irrigated', project=
 
     # debug_flag=False just returns the ndarray for writing
     if not debug_flag:
+        eta, swe = df
         for i, fid in enumerate(fields.input['order']):
-            pred = df[:, 0, i]
-            np.savetxt(os.path.join(d, 'pest', 'obs', 'obs_eta_{}.np'.format(fid)), pred)
+            pred_eta, pred_swe = eta[:, 0, i], swe[:, 0, i]
+            np.savetxt(os.path.join(d, 'pest', 'pred', 'pred_eta_{}.np'.format(fid)), pred_eta)
+            np.savetxt(os.path.join(d, 'pest', 'pred', 'pred_swe_{}.np'.format(fid)), pred_swe)
             end_time = time.time()
         print('\n\nExecution time: {:.2f} seconds'.format(end_time - start_time))
 
@@ -36,33 +38,44 @@ def optimize_fields(ini_path, debug_flag=False, field_type='irrigated', project=
 
         for fid in targets:
 
-            pred = df[fid]['et_act'].values
+            pred_et = df[fid]['et_act'].values
 
-            obs = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}/obs/obs_eta_{}.np'.format(project, fid)
-            obs = np.loadtxt(obs)
+            obs_et = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}/obs/obs_eta_{}.np'.format(project, fid)
+            obs_et = np.loadtxt(obs_et)
             cols = ['et_obs'] + list(df[fid].columns)
-            df[fid]['et_obs'] = obs
+            df[fid]['et_obs'] = obs_et
             df[fid] = df[fid][cols]
             a = df[fid].loc['2010-01-01': '2021-01-01']
 
-            comp = pd.DataFrame(data=np.vstack([obs, pred]).T, columns=['obs', 'pred'], index=df[fid].index)
+            comp = pd.DataFrame(data=np.vstack([obs_et, pred_et]).T, columns=['obs', 'pred'], index=df[fid].index)
             comp['eq'] = comp['obs'] == comp['pred']
             comp['capture'] = df[fid]['capture']
 
-            rmse = np.sqrt(((pred - obs) ** 2).mean())
+            rmse = np.sqrt(((pred_et - obs_et) ** 2).mean())
             end_time = time.time()
 
             if first:
                 print('Execution time: {:.2f} seconds'.format(end_time - start_time))
                 first = False
 
-            print('{}: Mean Obs: {:.2f}, Mean Pred: {:.2f}'.format(fid, obs.mean(), pred.mean()))
+            print('{}: Mean Obs: {:.2f}, Mean Pred: {:.2f}'.format(fid, obs_et.mean(), pred_et.mean()))
             print('{}: RMS Diff: {:.4f}'.format(fid, rmse))
 
             comp = comp.loc[a[a['capture'] == 1.0].index]
-            pred, obs = comp['pred'], comp['obs']
-            rmse = np.sqrt(((pred - obs) ** 2).mean())
-            print('{}: RMSE Capture Dates: {:.4f}\n\n\n\n'.format(fid, rmse))
+            pred_et, obs_et = comp['pred'], comp['obs']
+            rmse = np.sqrt(((pred_et - obs_et) ** 2).mean())
+            print('{}: RMSE Capture Dates: {:.4f}'.format(fid, rmse))
+
+            obs_swe = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}/obs/obs_swe_{}.np'.format(project, fid)
+            obs_swe = np.loadtxt(obs_swe)
+            cols = ['swe_obs'] + list(df[fid].columns)
+            df[fid]['swe_obs'] = obs_swe
+            df[fid] = df[fid][cols]
+            swe_df = df[fid].loc['2010-01-01': '2021-01-01'][['swe_obs', 'swe']]
+            pred_swe = swe_df['swe'].values
+            obs_swe = swe_df['swe_obs'].values
+            rmse = np.sqrt(((pred_swe - obs_swe) ** 2).mean())
+            print('{}: RMSE SWE: {:.4f}\n\n\n\n'.format(fid, rmse))
 
 
 if __name__ == '__main__':
@@ -70,4 +83,4 @@ if __name__ == '__main__':
     field_type_ = 'irrigated'
     d = '/home/dgketchum/PycharmProjects/swim-rs/examples/{}'.format(project_)
     ini = os.path.join(d, '{}_swim.toml'.format(project_))
-    optimize_fields(ini_path=ini, debug_flag=True, field_type=field_type_, project=project_)
+    optimize_fields(ini_path=ini, debug_flag=False, field_type=field_type_, project=project_)
