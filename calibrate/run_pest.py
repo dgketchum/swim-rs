2e28dad40eaf404761c9e1ea2d631d68881a53d6
dgketchum <dgketchum@gmail.com>
2024-04-09 10:40:12 -0600

    run PEST++ from python

--- calibrate/run_pest.py
+++ calibrate/run_pest.py
@@ -1,13 +1,19 @@
 import os
+import shutil
+
 import psutil
 
-from pyemu import Pst, os_utils
+from pyemu import os_utils
 
 
-def run_pst(_dir, _cmd, pst, num_workers, worker_root, master_dir):
-    os_utils.start_workers(_dir, _cmd, pst,
+def run_pst(_dir, _cmd, pst_file, num_workers, worker_root, master_dir=None, verbose=False):
+
+    [shutil.rmtree(os.path.join(_dir, d)) for d in os.listdir(_dir) if 'worker' in d]
+
+    os_utils.start_workers(_dir, _cmd, pst_file,
                            num_workers=num_workers,
                            worker_root=worker_root,
+                           verbose=verbose,
                            master_dir=master_dir)
 
 
@@ -19,5 +25,5 @@ if __name__ == '__main__':
     _workers = psutil.cpu_count(logical=False) - 2
     worker_rt = os.path.join(d)
 
-    run_pst(d, exe_, _pst, num_workers=_workers, worker_root=worker_rt, master_dir='ies')
+    run_pst(d, exe_, _pst, num_workers=_workers, worker_root=worker_rt, verbose=True)
 # ========================= EOF ====================================================================
