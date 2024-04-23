aaebeeaa274244a8b62f6fb608614582aace41d9
dgketchum <dgketchum@gmail.com>
2024-04-23 16:39:17 -0600

    remove curve number runoff function

--- model/etd/runoff.py
+++ model/etd/runoff.py
@@ -1,5 +1,4 @@
 """runoff.py
-Defines function runoff for computing SCS curve number runoff
 called by compute_crop_et.py
 
 """
@@ -9,6 +8,7 @@ import warnings
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-def runoff_infiltration_excess(foo, foo_day, debug_flag=False):
+
+def runoff_infiltration_excess(foo, foo_day):
     foo.sro = np.maximum(foo_day.hr_precip - foo.ksat_hourly,
                          np.zeros_like(foo_day.hr_precip)).sum(axis=0).reshape(1, -1)
