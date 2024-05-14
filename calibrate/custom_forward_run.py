47548cfc1f81e43fd865287fc15c070de975f3f6
dgketchum <dgketchum@gmail.com>
2024-05-14 13:56:37 -0600

    update custom forward run

--- calibrate/custom_forward_run.py
+++ calibrate/custom_forward_run.py
@@ -6,9 +6,9 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 def run():
-    p = '/home/dgketchum/PycharmProjects/swim-rs/run/run_field_etd_mp.py'
+    p = '/home/dgketchum/PycharmProjects/swim-rs/run/run_mp.py'
     python_path = '/home/dgketchum/PycharmProjects/swim-rs'
-    conf_file = '/home/dgketchum/PycharmProjects/swim-rs/examples/tongue/tongue_swim.toml'
+    conf_file = '/home/dgketchum/PycharmProjects/swim-rs/examples/flux/flux_swim.toml'
     os.environ['PYTHONPATH'] = python_path
     args = ['python' + ' {}'.format(p), '--config_path', conf_file, '--worker_dir', os.getcwd()]
     os.system(' '.join(args))
