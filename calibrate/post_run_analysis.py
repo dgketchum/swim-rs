87f508c0e94c4f567a0b0946c4aad0a42e16f7fa
dgketchum <dgketchum@gmail.com>
2024-05-28 17:23:43 -0600

    add vlines and flux scatter to time series

--- calibrate/post_run_analysis.py
+++ calibrate/post_run_analysis.py
@@ -6,14 +6,24 @@ from pyemu import Pst, ObservationEnsemble, Ensemble
 
 import matplotlib.pyplot as plt
 
+from bokeh.plotting import figure, output_file, show, save
+from bokeh.layouts import column
+from bokeh.io import output_notebook
+from bokeh.palettes import Category10
 
-def plot_tseries_ensembles(pst_dir, glob='tongue', target=1779, sample_n=30):
+from bokeh.models import DatetimeTickFormatter, Span
+from datetime import datetime, timedelta
+
+
+def plot_tseries_ensembles(pst_dir, glob='tongue', target=1779, sample_n=30, idx_start=None, idx_end=None,
+                           flux_df=None, start_date='2000-01-01'):
     pst = Pst(os.path.join(pst_dir, '{}.pst'.format(glob)))
 
     def reduce_obs_ens(obj):
         obj = pd.DataFrame(obj.T.values, obj.T.index)
         obj['time'] = [int(i.split(':')[-2].split('_')[0]) for i in obj.index]
-        obj = obj.loc[[i for i, t in obj['time'].items() if t in list(range(6210, 6575))]]
+        if idx_start and idx_end:
+            obj = obj.loc[[i for i, t in obj['time'].items() if t in list(range(idx_start, idx_end))]]
         obj['oname'] = [i.split(':')[1].split('_')[1] for i in obj.index]
         obj = obj.loc[[i for i in obj.index if str(target.lower()) in i]]
         return obj
@@ -30,43 +40,73 @@ def plot_tseries_ensembles(pst_dir, glob='tongue', target=1779, sample_n=30):
 
     obs = pst.observation_data.copy()
     obs['time'] = [int(i.split(':')[-2].split('_')[0]) for i in obs.index]
-    obs = obs.loc[[i for i, t in obs['time'].items() if t in list(range(6210, 6575))]]
+    if idx_start and idx_end:
+        obs = obs.loc[[i for i, t in obs['time'].items() if t in list(range(idx_start, idx_end))]]
     obs = obs.loc[[i for i in obs.index if str(target.lower()) in i]]
     obs['oname'] = [i.split(':')[1].split('_')[1] for i in obs.index]
     obs['obgnme'] = obs['oname'].copy()
     ogs = obs.obgnme.unique()
-    fig, axes = plt.subplots(len(ogs), 1, figsize=(10, 2 * len(ogs)))
     ogs.sort()
 
+    if not flux_df:
+        flux_df = []
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+    # last two columns of prior and posterior results are 'time' and 'oname'
+    samples = np.random.choice(pt_oe.columns[:-2], sample_n, replace=False)
+
+    plots = []
+    colors = Category10[10]
+
     # for each observation group (i.e. timeseries)
-    for ax, og in zip(axes, ogs):
+    for og in ogs:
         # get values for x axis
         oobs = obs.loc[obs.obgnme == og, :].copy()
         oobs.loc[:, 'time'] = oobs.time.astype(float)
         oobs.sort_values(by='time', inplace=True)
-        tvals = oobs.time.values
-        onames = oobs.obsnme.values
+        tvals = [start_date + timedelta(days=int(t)) for t in oobs.time.values]
+
+        p = figure(title=og, x_axis_label='Time', y_axis_label='Value', width=2400, height=800, x_axis_type="datetime")
+
         # plot prior
-        [ax.plot(tvals, pr_oe.loc[pr_oe.oname == og, i].values, '0.5', lw=0.5, alpha=0.5, label='prior')
-         for i in range(0, sample_n)]
+        for i in samples:
+            p.line(tvals, pr_oe.loc[pr_oe.oname == og, i].values, line_width=0.5, alpha=0.3, color=colors[0],
+                   legend_label='Prior')
+
         # plot posterior
-        [ax.plot(tvals, pt_oe.loc[pr_oe.oname == og, i].values, 'b', lw=0.5, alpha=0.5, label='posterior')
-         for i in range(0, sample_n)]
-        # plot measured+noise 
-        # oobs = oobs.loc[oobs.weight > 0, :]
-        tvals = oobs.time.values
-        onames = oobs.obsnme.values
-        [ax.plot(tvals, noise.loc[noise.oname == og, i].values, 'r', lw=0.5, alpha=0.5, label='obs + noise')
-         for i in range(0, sample_n)]
-        ax.plot(oobs.time, oobs.obsval, 'r-', lw=2)
-        ax.set_title(og, loc='left')
+        for i in samples:
+            p.line(tvals, pt_oe.loc[pr_oe.oname == og, i].values, line_width=0.5, alpha=0.5,
+                   color=colors[1], legend_label='Posterior')
 
-        if og == 'swe':
-            ax.set_ylim(pt_oe[[i for i in range(0, sample_n)]].values.min(),
-                        pt_oe[[i for i in range(0, sample_n)]].values.max())
+        p.line(tvals, oobs.obsval, line_width=2, color='red', legend_label='Observed')
 
-    fig.tight_layout()
-    plt.show()
+        p.legend.location = "top_left"
+        p.xaxis.formatter = DatetimeTickFormatter(days=["%Y-%m-%d"], months=["%Y-%m-%d"], years=["%Y-%m-%d"])
+        plots.append(p)
+
+        if flux_df is not None and og == 'eta':
+            end_date = start_date + timedelta(days=len(tvals) - 1)
+            dt_index = pd.DatetimeIndex(pd.date_range(start_date, end_date, freq='D'))
+            flux_df = pd.read_csv(flux_df, index_col=0, parse_dates=True)
+            flux_df = flux_df.reindex(dt_index)
+            p.scatter(tvals, flux_df['ET'], size=6, color='black', legend_label='Flux Obs')
+            p.line(tvals, flux_df['ET_fill'], line_width=2, color='green', legend_label='Flux Fill')
+
+        if og == 'eta':
+            for time in obs.loc[obs['weight'] == 1.0, 'time']:
+                vline = Span(location=start_date + timedelta(days=int(time)), dimension='height', line_color='red',
+                             line_width=1, line_dash='dashed')
+                p.add_layout(vline)
+
+    # Create a layout and show/save the plot
+    plot_dir = os.path.join(pest_dir_, 'plots')
+    if not os.path.isdir(plot_dir):
+        os.mkdir(plot_dir)
+
+    _fig_file = os.path.join(plot_dir, 'timeseries_ensembles.html')
+    output_file(_fig_file)
+    save(column(*plots))
 
 
 def plot_prediction_scatter(pst_dir, glob='tongue', target=1779, sample_n=30):
@@ -176,14 +216,20 @@ if __name__ == '__main__':
     project = 'flux'
     pest_root = 'master'
 
-    src = '/home/dgketchum/PycharmProjects/swim-rs'.format(project)
+    src = '/home/dgketchum/PycharmProjects/swim-rs'
     d = os.path.join(src, 'examples/{}'.format(project))
+
+    site = 'US-Mj1'.upper()
+    flux_obs_ = os.path.join('/media/research/IrrigationGIS/climate/flux_ET_dataset/'
+                             'daily_data_files/{}_daily_data.csv'.format(site))
+
+    # d = '/media/research/IrrigationGIS/swim/optimized/flux/us-mj1'
 
     pest_dir_ = os.path.join(d, pest_root)
     pst_f = os.path.join(pest_dir_, '{}.pst'.format(project))
 
-    show_phi_evolution(pest_dir_, glob=project)
+    # show_phi_evolution(pest_dir_, glob=project)
 
-    # plot_tseries_ensembles(pest_dir_, glob=project, target='US-MC1', sample_n=30)
-    # plot_prediction_scatter(pest_dir_, glob=project, target='US-MC1', sample_n=30)
+    plot_tseries_ensembles(pest_dir_, glob=project, target=site, sample_n=30, flux_df=flux_obs_)
+    # plot_prediction_scatter(pest_dir_, glob=project, target='US-MJ1', sample_n=30)
 # ========================= EOF ====================================================================
