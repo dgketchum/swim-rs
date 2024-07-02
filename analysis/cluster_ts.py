a93f7b7e826915d3801de4aa49ab478e0a3fa7a8
dgketchum <dgketchum@gmail.com>
2024-07-02 10:30:48 -0600

    make a large sample of ndvi signals

--- analysis/cluster_ts.py
+++ analysis/cluster_ts.py
@@ -1,20 +1,15 @@
-import os
 import json
+import os
+import random
 
-from tqdm import tqdm
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib.ticker import StrMethodFormatter
 
-from tslearn.clustering import KShape
-from tslearn.datasets import CachedDatasets
-from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 
-
-def cluster_ndvi(csv_dir, out_json, fig_dir):
-
+def cluster_ndvi(csv_dir, out_json, fig_dir, sample_n=1000):
     l = [os.path.join(csv_dir, _file) for _file in os.listdir(csv_dir)]
+    random.shuffle(l)
     dct = {}
 
     for ndvi in ['ndvi_irr', 'ndvi_inv_irr']:
@@ -37,12 +32,12 @@ def cluster_ndvi(csv_dir, out_json, fig_dir):
             else:
                 mdf = pd.concat([mdf, ydf], axis=1)
 
-            if len(mdf.columns) > 1000:
+            if len(mdf.columns) > 5000:
                 break
 
         colors = ['k' for _ in mdf.columns]
         years = list(set([int(c.split('_')[-1]) for c in mdf.columns]))
-        ax = mdf.plot(logy=False, legend=False, alpha=0.2, color=colors, ylabel='NDVI',
+        ax = mdf.plot(logy=False, legend=False, alpha=0.1, color=colors, ylabel='NDVI',
                       title='{} - {}'.format(years[0], years[-1]), figsize=(30, 10))
 
         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:.2f}'))
@@ -61,39 +56,16 @@ def cluster_ndvi(csv_dir, out_json, fig_dir):
         json.dump(dct, f)
 
 
-def cluster_growing_seasons(_dir, plot_fig):
-    seed = 0
-    np.random.seed(seed)
-    X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
-    # Keep first 3 classes and 50 first time series
-    X_train = X_train[y_train < 4]
-    X_train = X_train[:50]
-    np.random.shuffle(X_train)
-    # For this method to operate properly, prior scaling is required
-    X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
-    sz = X_train.shape[1]
-
-    # kShape clustering
-    ks = KShape(n_clusters=3, verbose=True, random_state=seed)
-    y_pred = ks.fit_predict(X_train)
-
-    plt.figure()
-    for yi in range(3):
-        plt.subplot(3, 1, 1 + yi)
-        for xx in X_train[y_pred == yi]:
-            plt.plot(xx.ravel(), "k-", alpha=.2)
-        plt.plot(ks.cluster_centers_[yi].ravel(), "r-")
-        plt.xlim(0, sz)
-        plt.ylim(-4, 4)
-        plt.title("Cluster %d" % (yi + 1))
-
-    plt.tight_layout()
-    plt.show()
-
-
 if __name__ == '__main__':
-    f_ = '/home/dgketchum/data/IrrigationGIS/swim/examples/tongue/input_timeseries'
-    c = '/home/dgketchum/data/IrrigationGIS/swim/examples/tongue/ts_cluster/plots'
-    j = '/home/dgketchum/data/IrrigationGIS/swim/examples/tongue/ts_cluster/median_ts.json'
-    cluster_ndvi(f_, j, c)
+    d = '/media/research/IrrigationGIS/swim'
+    if not os.path.exists(d):
+        d = '/home/dgketchum/data/IrrigationGIS/swim'
+
+    project = 'tongue'
+    data = os.path.join(d, 'examples', project)
+
+    f_ = os.path.join(data, 'input_timeseries')
+    c = os.path.join(data, 'ts_cluster', 'plots')
+    j = os.path.join(data, 'ts_cluster', 'median_ts.json')
+    cluster_ndvi(f_, j, c, sample_n=15000)
 # ========================= EOF ====================================================================
