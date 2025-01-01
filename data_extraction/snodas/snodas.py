852c8c9ffdbab54d1b7212b9dd2628a94759ba50
dgketchum <dgketchum@gmail.com>
2024-12-31 20:11:17 -0700

    remove zonal stats functionality and write ee extracts to dict

--- data_extraction/snodas/snodas.py
+++ data_extraction/snodas/snodas.py
@@ -1,42 +1,27 @@
-import os
 import json
+import glob
 
-import geopandas as gpd
+import pandas as pd
+from tqdm import tqdm
 
-from rasterstats import zonal_stats
+def create_timeseries_json(directory, json_out, feature_id='FID'):
+    all_files = glob.glob(directory + "/*.csv")
+    timeseries = {}
+    for filename in tqdm(all_files, desc="Processing CSV files"):
+        df = pd.read_csv(filename, index_col=feature_id)
+        for index, row in df.iterrows():
+            fid = index
+            if fid not in timeseries:
+                timeseries[fid] = []
+            for date, value in row.items():
+                timeseries[fid].append({'date': date, 'value': value})
 
-
-def snodas_zonal_stats(in_shp, raster_dir, out_js, targets=None, index_col='FID'):
-    df = gpd.read_file(in_shp)
-    df.index = [i for i in df[index_col]]
-
-    if targets:
-        df = df.loc[targets]
-
-    geo, fids = list(df['geometry']), list(df[index_col])
-
-    l = sorted([os.path.join(raster_dir, x) for x in os.listdir(raster_dir) if x.endswith('.tif')])
-
-    dct = {}
-
-    print('Extracting snodas for {} objects over {} rasters'.format(len(fids), len(l)))
-    for r in l:
-        dts = os.path.basename(r).replace('.tif', '').split('_')[-1]
-        dct[dts] = {}
-        stats = zonal_stats(geo, r, stats=['mean'], all_touched=True)
-        for fid, s in zip(fids, stats):
-            if s['mean']:
-                dct[dts][fid] = float(s['mean'])
-            else:
-                dct[dts][fid] = 0.0
-        print(os.path.basename(r), dct[dts][fid])
-
-    with open(out_js, 'w') as fp:
-        json.dump(dct, fp, indent=4)
-
-    print('wrote', out_js)
+    with open(json_out, 'w') as f:
+        json.dump(timeseries, f)
+        print(f'wrote {json_out}')
 
 
 if __name__ == '__main__':
     pass
+
 # ========================= EOF ====================================================================
