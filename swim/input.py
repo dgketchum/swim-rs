0f8a5f14332d0ef76fcbf27c095202078fb64865
dgketchum <dgketchum@gmail.com>
2025-01-03 09:01:31 -0700

    add method to convert selected sample to datetime indexed df

--- swim/input.py
+++ swim/input.py
@@ -1,8 +1,11 @@
 import json
 
+import numpy as np
+import pandas as pd
+
 
 class SamplePlots:
-    """A Container for input time series, historical, and static field information
+    """A Container for input and output time series, historical, and static field information
 
     This should include some initial estimate of soil properties and historical
     estimate of irrigated status and crop type.
@@ -18,6 +21,28 @@ class SamplePlots:
         f = config.input_data
         with open(f, 'r') as fp:
             self.input = json.load(fp)
+
+    def input_to_dataframe(self, feature_id):
+
+        idx = self.input['order'].index(feature_id)
+
+        ts = self.input['time_series']
+        dct = {k: [] for k in ts[list(ts.keys())[0]]}
+        dates = []
+
+        for dt in ts:
+            doy_data = ts[dt]
+            dates.append(dt)
+            for k, v in doy_data.items():
+                if k == 'doy':
+                    dct['doy'].append(v)
+                else:
+                    # all other values are lists
+                    dct[k].append(v[idx])
+
+        df_ = pd.DataFrame().from_dict(dct)
+        df_.index = pd.DatetimeIndex(dates)
+        return df_
 
 
 if __name__ == '__main__':
