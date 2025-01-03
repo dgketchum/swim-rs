bf29eec89bde2261b8194e42e7b41b71fa9e130b
dgketchum <dgketchum@gmail.com>
2025-01-02 20:00:59 -0700

    refactor join_daily_timeseries

--- prep/field_timeseries.py
+++ prep/field_timeseries.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -13,7 +14,7 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, overwrite=False,
-                          start_date=None, end_date=None, feature_id='FID',  **kwargs):
+                          start_date=None, end_date=None, feature_id='FID', **kwargs):
     with open(snow, 'r') as f:
         snow = json.load(f)
 
@@ -28,9 +29,9 @@ def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, ove
     field_df = gpd.read_file(fields)
     field_df.index = field_df[feature_id]
 
-    out_plots = []
+    out_plots, bad = [], None
 
-    for f, row in field_df.iterrows():
+    for f, row in tqdm(field_df.iterrows(), total=field_df.shape[0]):
 
         if 'target_fields' in kwargs.keys():
             if str(f) not in kwargs['target_fields']:
@@ -48,7 +49,7 @@ def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, ove
             gridmet = pd.read_csv(gridmet_file, index_col='date', parse_dates=True).loc[start: end]
 
             time_covered = ((pd.to_datetime(start_date) >= gridmet.index[0]) &
-                            (pd.to_datetime(end_date) >= gridmet.index[-1]))
+                            (pd.to_datetime(end_date) <= gridmet.index[-1]))
 
             if os.path.exists(_file) and not overwrite and time_covered:
                 continue
@@ -56,15 +57,16 @@ def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, ove
             if not time_covered:
                 bias_factors = fields.replace('.shp', '.json')
                 download_gridmet(fields, bias_factors, gridmet_dir, start_date, end_date,
-                                 overwrite=True, target_fields=[f])
+                                 overwrite=True, target_fields=[f], feature_id=feature_id)
 
             drop_cols = [c for c in gridmet.columns if '.' in c]
             if drop_cols:
                 gridmet.drop(columns=drop_cols, inplace=True)
 
-            swe_data = [(pd.to_datetime(k), v[str(f)]) for k, v in snow.items()]
+            swe_data = [(pd.to_datetime(d['date']), d['value']) for d in snow[f]]
             swe = pd.Series(index=[x[0] for x in swe_data], data=[x[1] for x in swe_data])
-            print('Max SWE {}: {}'.format(f, swe.max()))
+            swe = swe.sort_index()
+
             match_idx = [i for i in gridmet.index if i in swe.index]
             gridmet.loc[match_idx, 'obs_swe'] = swe
 
@@ -84,75 +86,55 @@ def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, ove
         if end_date:
             gridmet = gridmet.loc[:end_date]
 
-        accept = True
+        accept, bad = True, 0
 
         for i, r in gridmet.iterrows():
             if np.isnan(r['ndvi_irr']) and np.isnan(r['ndvi_inv_irr']):
                 print('{} in {} has only nan in ndvi_irr and ndvi_inv_irr'.format(f, i.year))
                 accept = False
+                bad += 1
                 break
             if np.isnan(r['etf_irr']) and np.isnan(r['etf_inv_irr']):
                 print('{} in {} has only nan in etf_irr and etf_inv_irr'.format(f, i.year))
                 accept = False
+                bad += 1
                 break
 
         if accept:
             gridmet.to_csv(_file)
-            print(_file)
-
             out_plots.append(f)
 
-    print(out_plots)
+    print(f'{len(out_plots)} fields were successfully processed')
+    print(f'{bad} fields were dropped due to missing data')
 
 
 if __name__ == '__main__':
+    root = '/home/dgketchum/PycharmProjects/swim-rs'
+    fields_gridmet = os.path.join(root, 'tutorial', 'step_3_meteorology_extract', 'mt_sid_boulder_gfid.shp')
+    met = os.path.join(root, 'tutorial', 'step_3_meteorology_extract', 'met_timeseries')
+    landsat = os.path.join(root, 'tutorial/step_2_earth_engine_extract/landsat/remote_sensing.csv')
+    snow = os.path.join(root, 'tutorial/step_2_earth_engine_extract/snodas/snodas.json')
 
-    d = '/media/research/IrrigationGIS/swim'
-    if not os.path.exists(d):
-        d = d = '/home/dgketchum/data/IrrigationGIS/swim'
-
-    project = 'flux'
-    project_ws = os.path.join(d, 'examples', project)
-
-    gridmet = os.path.join(d, 'gridmet')
-    rasters_ = os.path.join(gridmet, 'gridmet_corrected', 'correction_surfaces_aea')
-    grimet_cent = os.path.join(gridmet, 'gridmet_centroids.shp')
-
-    fields_shp = os.path.join(project_ws, 'gis', '{}_fields.shp'.format(project))
-
-    fields_gridmet = os.path.join(project_ws, 'gis', '{}_fields_gfid.shp'.format(project))
-    gridmet_factors = os.path.join(project_ws, 'gis', '{}_fields_gfid.json'.format(project))
-
-    met = os.path.join(project_ws, 'met_timeseries')
-
-    # targets = FLUX_SELECT
-    targets = None
-
-    find_gridmet_points(fields_shp, grimet_cent, rasters_, fields_gridmet, gridmet_factors, field_select=targets)
-
-    download_gridmet(fields_gridmet, gridmet_factors, met, start='1987-01-01', end='2023-12-31',
-                     target_fields=targets, overwrite=False)
-
-    fields_shp_wgs = os.path.join(project_ws, 'gis', '{}_fields_wgs.shp'.format(project))
-    snow_ts = os.path.join(project_ws, 'snow_timeseries', 'snodas_{}.json'.format(project))
-
-    # this extract is meant to be run on Montana Climate Office machine (Zoran)
-    s_dir = '/data/ssd1/snodas/processed/swe'
-    if not os.path.isdir(s_dir):
-        s_dir = '/media/research/IrrigationGIS/climate/snodas/processed/swe'
-    # snodas_zonal_stats(fields_shp_wgs, s_dir, snow_ts, targets=None, index_col='FID')
-
-    landsat = os.path.join(project_ws, 'landsat', '{}_sensing.csv'.format(project))
-    dst_dir_ = os.path.join(project_ws, 'input_timeseries')
+    joined_timeseries = os.path.join(root, 'tutorial/step_4_model_data_prep/input_timeseries')
+    if not os.path.isdir(joined_timeseries):
+        os.mkdir(joined_timeseries)
+    shapefile_path = os.path.join(root, 'tutorial/step_1_domain/mt_sid_boulder.shp')
 
     params = ['etf_inv_irr',
               'ndvi_inv_irr',
               'etf_irr',
               'ndvi_irr']
-
     params += ['{}_ct'.format(p) for p in params]
 
-    join_daily_timeseries(fields_gridmet, met, landsat, snow_ts, dst_dir_, overwrite=False,
-                          start_date='1989-01-01', end_date='2021-12-31', **{'params': params})
+    join_daily_timeseries(fields=fields_gridmet,
+                          gridmet_dir=met,
+                          landsat_table=landsat,
+                          snow=snow,
+                          dst_dir=joined_timeseries,
+                          overwrite=False,
+                          start_date='2004-01-01',
+                          end_date='2022-12-31',
+                          feature_id='FID_1',
+                          **{'params': params})
 
 # ========================= EOF ====================================================================
