d6c53549162a45a1c5032ba3be9699477941843c
dgketchum <dgketchum@gmail.com>
2025-01-06 08:46:06 -0700

    etf comparisons in tutorial runner script

--- run/run_tutorial.py
+++ run/run_tutorial.py
@@ -1,7 +1,9 @@
 import os
 import time
 
+import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
 
 from model.etd import obs_field_cycle
 from swim.config import ProjectConfig
@@ -27,25 +29,65 @@ def run_fields(ini_path, project_ws, selected_feature, output_csv):
     in_df = fields.input_to_dataframe(selected_feature)
 
     df = pd.concat([out_df, in_df], axis=1, ignore_index=False)
+    df = df.loc[config.start_dt:config.end_dt]
+
     df.to_csv(output_csv)
+    print(df.shape)
+
+
+def compare_etf_estimates(combined_output_path, flux_data_path, irr=False):
+    """"""
+    flux_data = pd.read_csv(flux_data_path, index_col='date', parse_dates=True)['EToF']
+
+    output = pd.read_csv(combined_output_path, index_col=0)
+    output.index = pd.to_datetime(output.index)
+
+    if irr:
+        etf, ct = 'etf_irr', 'etf_irr_ct'
+    else:
+        etf, ct = 'etf_inv_irr', 'etf_inv_irr_ct'
+
+    df = pd.DataFrame({'kc_act': output['kc_act'],
+                       'etf': output[etf],
+                       'ct': output[ct],
+                       'EToF': flux_data})
+
+    # filter for days that have a SSEBop ETf retrieval and a flux observation
+    df = df.dropna()
+    df = df.loc[df['ct'] == 1]
+
+    # Calculate RMSE and R-squared
+    rmse_kc_act = np.sqrt(mean_squared_error(df['EToF'], df['kc_act']))
+    r2_kc_act = r2_score(df['EToF'], df['kc_act'])
+
+    rmse_ssebop = np.sqrt(mean_squared_error(df['EToF'], df['etf']))
+    r2_ssebop = r2_score(df['EToF'], df['etf'])
+
+    print(f"SWIM Kc_act vs. Flux EToF: RMSE = {rmse_kc_act:.2f}, R-squared = {r2_kc_act:.2f}")
+    print(f"SSEBop ETf vs. Flux EToF: RMSE = {rmse_ssebop:.2f}, R-squared = {r2_ssebop:.2f}")
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
     root = os.path.join(home, 'PycharmProjects', 'swim-rs')
-    config_file = os.path.join(root, 'tutorials', '1_Boulder', 'data', 'tutorial_config.toml')
-    project_ws_ = os.path.join(root, 'tutorials', '1_Boulder')
 
-    selected_feature = '043_000128'
-    out_csv = os.path.join(root, 'tutorials', '1_Boulder', 'step_5_model_run',
-                           f'combined_output_{selected_feature}.csv')
+    # project_ws = os.path.join(root, 'tutorials', '2_Fort_Peck')
+    project_ws = os.path.join(root, 'tutorials', '3_Crane')
 
-    run_fields(config_file, project_ws_, selected_feature=selected_feature, output_csv=out_csv)
+    data = os.path.join(project_ws, 'data')
 
-    start = '2007-01-01'
-    end = '2009-12-31'
-    plot_swim_timeseries(out_csv, ['snow_fall', 'rain', 'melt', 'dperc'], start=start, end=end)
+    config_file = os.path.join(data, 'tutorial_config.toml')
+    prepped_input = os.path.join(data, 'prepped_input.json')
 
-    # plot_timeseries(out_csv, ['et_act', 'etref', 'irrigation'], start=start, end=end)
+    selected_feature = 'S2'
+    out_csv = os.path.join(project_ws, 'step_2_uncalibrated_model', f'combined_output_{selected_feature}.csv')
+
+    run_fields(config_file, project_ws, selected_feature=selected_feature, output_csv=out_csv)
+
+    # plot_swim_timeseries(out_csv, ['et_act', 'etref', 'rain', 'melt', 'irrigation'],
+    #                      start='2021-01-01', end='2021-12-31', png_file='et.png')
+
+    # flux_data = os.path.join(data, f'{selected_feature}_daily_data.csv')
+    # compare_etf_estimates(out_csv, flux_data, irr=True)
 
 # ========================= EOF ====================================================================
