a53758a722d7187a63029a383aa85400a052c25e
dgketchum <dgketchum@gmail.com>
2025-01-26 15:11:13 -0700

    generalize forward run script paths determination

--- calibrate/custom_forward_run.py
+++ calibrate/custom_forward_run.py
@@ -5,29 +5,43 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 
+def split_path(path):
+    parts = []
+    while True:
+        path, folder = os.path.split(path)
+        if folder:
+            parts.append(folder)
+        else:
+            if path:
+                parts.append(path)
+            break
+    return parts[::-1]
+
+
 def run():
     """This script is meant to be executed by PEST++"""
-    home = os.path.expanduser('~')
 
-    root = os.path.join(home, 'PycharmProjects', 'swim-rs')
+    path = split_path(__file__)
+    project_top = path.index('swim-rs')
+    root = os.path.join(*path[:project_top + 1])
+
     os.environ['PYTHONPATH'] = root
 
     model_script = os.path.join(root, 'run', 'run_mp.py')
 
-    project_ws = os.path.join(root, 'tutorials', '4_Flux_Network')
+    project_ws = os.path.join(root, 'tutorials', 'DUMMY_PROJECT')
 
-    conf_file = os.path.join(project_ws, 'data', 'tutorial_config.toml')
+    conf_file = os.path.join(project_ws, 'config.toml')
 
     cwd = os.getcwd()
+
+    calibration_dir = os.path.join(cwd, 'mult')
 
     args = ['python' + ' {}'.format(model_script),
             '--project_dir', project_ws,
             '--config_path', conf_file,
-            '--worker_dir', cwd]
-
-    if 'worker' in os.path.basename(cwd):
-        calibration_dir = os.path.join(os.getcwd(), 'mult')
-        args += ['--calibration_dir', calibration_dir]
+            '--worker_dir', cwd,
+            '--calibration_dir', calibration_dir]
 
     os.system(' '.join(args))
 
