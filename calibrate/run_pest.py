0a102484f4685085a2dcb79007ebec19ef2390c0
dgketchum <dgketchum@gmail.com>
2025-01-26 15:12:37 -0700

    panther working on generalized forward run script and builder class-produced PEST files

--- calibrate/run_pest.py
+++ calibrate/run_pest.py
@@ -11,33 +11,35 @@ def run_pst(_dir, _cmd, pst_file, num_workers, worker_root, master_dir=None, ver
         [print('rmtree: {}'.format(os.path.join(worker_root, d))) for d in os.listdir(worker_root)]
         [shutil.rmtree(os.path.join(worker_root, d)) for d in os.listdir(worker_root)]
     except FileNotFoundError:
-        os.mkdir(os.path.join(worker_root))
+        os.mkdir(worker_root)
         if master_dir:
-            os.mkdir(os.path.join(master_dir))
+            os.mkdir(master_dir)
 
-    os_utils.start_workers(_dir, _cmd, pst_file,
+    os_utils.start_workers(_dir,
+                           _cmd,
+                           pst_rel_path=pst_file,
                            num_workers=num_workers,
                            worker_root=worker_root,
                            verbose=verbose,
                            master_dir=master_dir,
                            cleanup=cleanup,
-                           port=5000)
+                           port=5005)
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
     root = os.path.join(home, 'PycharmProjects', 'swim-rs')
-    project_ws = os.path.join(root, 'tutorials', '2_Fort_Peck')
+    project_ws = os.path.join(root, 'tutorials', '3_Crane')
 
     p_dir = os.path.join(project_ws, 'pest')
     m_dir = os.path.join(project_ws, 'master')
     w_dir = os.path.join(project_ws, 'workers')
     exe_ = 'pestpp-ies'
 
-    _pst = os.path.join(project_ws, 'pest', '2_Fort_Peck.pst')
+    _pst = '3_Crane.pst'
 
-    _workers = 4
+    _workers = 2
 
     run_pst(p_dir, exe_, _pst, num_workers=_workers, worker_root=w_dir,
-            master_dir=m_dir, verbose=False)
+            master_dir=m_dir, verbose=True, cleanup=False)
 # ========================= EOF ====================================================================
