75210458b06920a16f8619bca76f779a1e956c5d
dgketchum <dgketchum@gmail.com>
2025-01-28 14:01:40 -0700

    working irrigation analysis, needs groundwater subsidy analysis still

--- prep/dynamics.py
+++ prep/dynamics.py
@@ -1,80 +1,102 @@
-import json
 import os
 
-import numpy as np
 import pandas as pd
+import numpy as np
+import json
 from tqdm import tqdm
 
 
-def plot_dynamics(landsat, irr_csv, out_json, irr_threshold=0.1, select=None):
-    lst = pd.read_csv(landsat, index_col=0, parse_dates=True)
-    years = list(set([i.year for i in lst.index]))
-    irr = pd.read_csv(irr_csv, index_col=0)
-    irr.drop(columns=['LAT', 'LON'], inplace=True)
+class SamplePlotDynamics:
+    def __init__(self, landsat_file, irr_csv_file, out_json_file, irr_threshold=0.1, select=None):
+        self.landsat_file = landsat_file
+        self.irr_csv_file = irr_csv_file
+        self.out_json_file = out_json_file
+        self.irr_threshold = irr_threshold
+        self.select = select
 
-    try:
-        _ = float(irr.index[0])
-        irr.index = [str(i) for i in irr.index]
-    except (TypeError, ValueError):
-        pass
+        self.lst = None
+        self.irr = None
+        self.years = None
+        self.fields = {}
 
-    irrigated, fields = False, {}
-    for c in tqdm(irr.index, desc='Analyzing Irrigation', total=len(irr.index)):
+        self._load_data()
 
-        if select and c not in select:
-            continue
 
-        if c not in irr.index:
-            print('{} not in index'.format(c))
-            continue
+    def analyze_irrigation(self):
 
-        if np.all(np.isnan(irr.loc[c])):
-            print('{} is all nan in {}'.format(c, irr_csv))
-            continue
+        for field in tqdm(self.irr.index, desc='Analyzing Irrigation', total=len(self.irr.index)):
+            if self.select and field not in self.select:
+                continue
 
-        fields[c] = {}
+            field_data = self._analyze_field_irrigation(field)
+            if field_data is not None:
+                self.fields[field] = field_data
 
-        selector = '{}_ndvi_irr'.format(c)
+    def save_results(self):
+        with open(self.out_json_file, 'w') as fp:
+            print(f'wrote {self.out_json_file}')
+            json.dump(self.fields, fp, indent=4)
 
+    def _load_data(self):
+        self.lst = pd.read_csv(self.landsat_file, index_col=0, parse_dates=True)
+        self.years = list(set([i.year for i in self.lst.index]))
+
+        self.irr = pd.read_csv(self.irr_csv_file, index_col=0)
+        self.irr.drop(columns=['LAT', 'LON'], inplace=True)
+
+        try:
+            _ = float(self.irr.index[0])
+            self.irr.index = [str(i) for i in self.irr.index]
+        except (TypeError, ValueError):
+            pass
+
+    def _analyze_field_irrigation(self, field):
+        if field not in self.irr.index:
+            print(f'{field} not in index')
+            return None
+
+        if np.all(np.isnan(self.irr.loc[field])):
+            print(f'{field} is all nan in {self.irr_csv_file}')
+            return None
+
+        field_data = {}
         fallow = []
 
-        for yr in years:
+        selector = f'{field}_ndvi_irr'
 
+        for yr in self.years:
             if yr > 2022:
                 continue
 
             irr_doys, periods = [], 0
 
             try:
-                f_irr = irr.at[c, 'irr_{}'.format(yr)]
+                f_irr = self.irr.at[field, f'irr_{yr}']
             except (ValueError, KeyError):
-                f_irr = irr.at[c, 'irr_{}'.format(yr)]
+                f_irr = np.nan
 
-            irrigated = f_irr > irr_threshold
+            irrigated = f_irr > self.irr_threshold
 
             if not irrigated:
                 fallow.append(yr)
-                fields[c][yr] = {'irr_doys': irr_doys,
-                                 'irrigated': int(irrigated),
-                                 'f_irr': f_irr}
+                field_data[yr] = {'irr_doys': irr_doys,
+                                  'irrigated': int(irrigated),
+                                  'f_irr': f_irr}
                 continue
 
-            df = lst.loc['{}-01-01'.format(yr): '{}-12-31'.format(yr), [selector]]
+            df = self.lst.loc[f'{yr}-01-01': f'{yr}-12-31', [selector]]
             df['doy'] = [i.dayofyear for i in df.index]
             df[selector] = df[selector].rolling(window=10, center=True).mean()
-            # df[selector] = savgol_filter(df[selector], window_length=7, polyorder=2)
 
             df['diff'] = df[selector].diff()
 
             nan_ct = np.count_nonzero(np.isnan(df[selector].values))
             if nan_ct > 200:
-                # print('{}: {} has {}/{} nan'.format(c, yr, nan_ct, df.shape[0]))
                 fallow.append(yr)
                 continue
 
             local_min_indices = df[(df['diff'] > 0) & (df['diff'].shift(1) < 0)].index
 
-            # Find periods with positive slope for more than 10 days
             positive_slope = (df['diff'] > 0)
             groups = (positive_slope != positive_slope.shift()).cumsum()
             df['groups'] = groups
@@ -82,20 +104,16 @@ def plot_dynamics(landsat, irr_csv, out_json, irr_threshold=0.1, select=None):
             long_positive_slope_groups = group_counts[group_counts >= 10].index
 
             for group in long_positive_slope_groups:
-                # Find the start and end indices of the group
                 group_indices = positive_slope[groups == group].index
                 start_index = group_indices[0]
                 end_index = group_indices[-1]
 
-                # Check if this group follows a local minimum
                 if start_index in local_min_indices:
                     start_doy = (start_index - pd.Timedelta(days=5)).dayofyear
                     end_doy = (end_index - pd.Timedelta(days=5)).dayofyear
                     irr_doys.extend(range(start_doy, end_doy + 1))
                     periods += 1
-
                 else:
-                    # Otherwise, just add the days within the group
                     start_doy = start_index.dayofyear
                     end_doy = (end_index - pd.Timedelta(days=5)).dayofyear
                     irr_doys.extend(range(start_doy, end_doy + 1))
@@ -103,17 +121,30 @@ def plot_dynamics(landsat, irr_csv, out_json, irr_threshold=0.1, select=None):
 
             irr_doys = sorted(list(set(irr_doys)))
 
-            fields[c][yr] = {'irr_doys': irr_doys,
-                             'irrigated': int(irrigated),
-                             'f_irr': f_irr}
+            field_data[yr] = {'irr_doys': irr_doys,
+                              'irrigated': int(irrigated),
+                              'f_irr': f_irr}
 
-        fields[c]['fallow_years'] = fallow
-
-    with open(out_json, 'w') as fp:
-        print('wrote {}'.format(out_json))
-        json.dump(fields, fp, indent=4)
+        field_data['fallow_years'] = fallow
+        return field_data
 
 
 if __name__ == '__main__':
-    pass
+
+    root = '/home/dgketchum/PycharmProjects/swim-rs'
+    data = os.path.join(root, 'tutorials', '4_Flux_Network', 'data')
+    shapefile_path = os.path.join(data, 'gis', 'flux_fields.shp')
+
+    irr = os.path.join(data, 'properties', 'calibration_irr.csv')
+    landsat = os.path.join(data, 'landsat')
+    remote_sensing_file = os.path.join(landsat, 'remote_sensing.csv')
+
+    FEATURE_ID = 'field_1'
+    selected_feature = None
+    cuttings_json = os.path.join(landsat, 'calibration_cuttings_.json')
+
+    dynamics = SamplePlotDynamics(remote_sensing_file, irr, irr_threshold=0.1,
+                                  out_json_file=cuttings_json, select='S2')
+    dynamics.analyze_irrigation()
+
 # ========================= EOF ====================================================================
