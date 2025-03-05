47fc7a23198df5abe632d03968b66fb176a88d79
dgketchum <dgketchum@gmail.com>
2025-03-05 11:05:23 -0700

    remove case where parameters and results read from group runs

--- analysis/results_comparison.py
+++ analysis/results_comparison.py
@@ -10,8 +10,9 @@ from swim.sampleplots import SamplePlots
 
 from run.run_tutorial import run_fields
 
+
 def compare_results(conf_path, project_ws, result_csv_dir, mode, summary_csv, cal_dir=None, select=None,
-                    updates=None):
+                    updates=None, overwrite=False, check_individual=False):
     """
     Compares results for different model modes, creating a summary table.
     Now handles daily, overpass, and monthly results separately.
@@ -33,8 +34,8 @@ def compare_results(conf_path, project_ws, result_csv_dir, mode, summary_csv, ca
 
     for fid, row in flux_meta_df.iterrows():
 
-        if fid != 'S2':
-            continue
+        # if fid != 'ALARC2_Smith6': # 'US-Blo'
+        #     continue
 
         out_csv, updated, fcst_file = None, False, None
 
@@ -53,48 +54,29 @@ def compare_results(conf_path, project_ws, result_csv_dir, mode, summary_csv, ca
 
         if not mode == 'uncal':
 
-            if updates:
-                fcst_file = os.path.join(updates, mode, fid, f'{fid}.3.par.csv')
-                if not os.path.exists(fcst_file):
-                    fcst_file = os.path.join(updates, mode, fid, f'{fid}.2.par.csv')
+            if updates is not None:
+                output_dir = os.path.join(updates, mode, fid)
 
-                if os.path.exists(fcst_file):
-                    input_file = os.path.join(updates, mode, fid, f'prepped_input_{fid}.json')
-                    output_dir = os.path.join(updates, mode, fid)
-                    out_csv = os.path.join(output_dir, f'{fid}.csv')
-                    if not os.path.exists(out_csv):
-                        print(f'\nRunning inference on updated parameters at {fid}')
-                        run_fields(config_file, project_ws, output_dir, forecast=True, forecast_file=fcst_file,
-                                   input_data=input_file)
-                    else:
-                        print(f'\nInference on updated parameters at {fid} exists, skipping new run')
-                    updated = True
+                fcst_file = os.path.join(output_dir, f'{fid}.3.par.csv')
+                if not os.path.exists(fcst_file):
+                    fcst_file = os.path.join(output_dir, f'{fid}.2.par.csv')
+
+                input_file = os.path.join(output_dir, f'prepped_input_{fid}.json')
+                out_csv = os.path.join(output_dir, f'{fid}.csv')
+                if not os.path.exists(out_csv) or overwrite:
+                    print(f'\nRunning inference on updated parameters at {fid}')
+                    run_fields(config_file, project_ws, output_dir, forecast=True, forecast_file=fcst_file,
+                               input_data=input_file)
                 else:
-                    fcst_file = None
-
-            if not fcst_file:
-                fcst_file = os.path.join(project_ws, f'{mode_}_master', f'{os.path.basename(project_ws)}.3.par.csv')
-                if not os.path.exists(fcst_file):
-                    fcst_file = os.path.join(project_ws, f'{mode_}_master', f'{os.path.basename(project_ws)}.2.par.csv')
-
-                pdc_file = os.path.join(cal_dir, f'{os.path.basename(project_ws)}.pdc.csv')
-
-                if os.path.exists(pdc_file):
-                    pdc = pd.read_csv(pdc_file, index_col=0)
-                    pdc_ct = pdc.shape[0]
-
-        if not out_csv:
-            updated = False
-            out_csv = os.path.join(result_csv_dir, f'{fid}.csv')
-            if not os.path.exists(out_csv):
-                print(f"WARNING: Model output file not found for {fid}")
-                continue
+                    print(f'\nInference on updated parameters at {fid} exists, skipping new run')
+                updated = True
+        else:
+            fcst_file = None
 
         print(f'\nProcessing {fid} for mode: {mode}')
 
         daily_results, overpass_results, monthly_results = compare_etf_estimates(
-            out_csv, flux_data, irr=irr_dct, target='et'
-        )
+            out_csv, flux_data, irr=irr_dct, target='et')
 
         site_results = {'fid': fid, 'mode': mode, 'lulc': row['General classification'],
                         'pdc': pdc_ct, 'updated': updated}
@@ -165,43 +147,43 @@ def compare_results(conf_path, project_ws, result_csv_dir, mode, summary_csv, ca
                 'monthly_r2_ssebop': np.nan,
                 'monthly_n_samples': np.nan
             })
+
+        if fcst_file:
+            param_dist = pd.read_csv(fcst_file, index_col=0)
+            param_mean = param_dist.mean(axis=0)
+            param_std = param_dist.std(axis=0)
+            p_str = ['_'.join(s.split(':')[1].split('_')[1:-1]) for s in list(param_mean.index)]
+            param_mean.index = p_str
+            param_std.index = p_str
+
+            group = None
+            for p_string in param_mean.index:
+                param_found = False
+                while not param_found:
+                    for p in TUNABLE_PARAMS:
+                        if p in p_string:
+                            group = p
+                            _fid = p_string.replace(f'{group}_', '')
+                            param_found = True
+
+                site_results[f'{group}_mean'] = param_mean[p_string]
+                site_results[f'{group}_std'] = param_std[p_string]
+        else:
+            size = len(fields.input['order'])
+
+            tracker = SampleTracker(size)
+            tracker.apply_parameters(config, fields, params=None)
+            tracker.load_root_depth(fields)
+            tracker.load_soils(fields)
+            fid_idx = fields.input['order'].index(fid)
+            for p in TUNABLE_PARAMS:
+                site_results[f'{p}_mean'] = tracker.__getattribute__(p)[0, fid_idx]
+                site_results[f'{p}_std'] = np.nan
+
         results_list.append(site_results)
 
     df_results = pd.DataFrame(results_list)
     df_results = df_results.set_index(['fid', 'mode'])
-
-    if fcst_file:
-        param_dist = pd.read_csv(fcst_file, index_col=0)
-        param_mean = param_dist.mean(axis=0)
-        param_std = param_dist.std(axis=0)
-        p_str = ['_'.join(s.split(':')[1].split('_')[1:-1]) for s in list(param_mean.index)]
-        param_mean.index = p_str
-        param_std.index = p_str
-
-        for p_string in param_mean.index:
-            param_found = False
-            while not param_found:
-                for p in TUNABLE_PARAMS:
-                    if p in p_string:
-                        group = p
-                        _fid = p_string.replace(f'{group}_', '')
-                        param_found = True
-
-            fid = [f for f in flux_meta_df.index if f.lower() == _fid][0]
-
-            df_results.loc[(fid, mode), f'{group}_mean'] = param_mean[p_string]
-            df_results.loc[(fid, mode), f'{group}_std'] = param_std[p_string]
-
-    else:
-        size = len(fields.input['order'])
-
-        tracker = SampleTracker(size)
-        tracker.load_soils(fields)
-        tracker.apply_parameters(config, fields, params=None)
-        for fid in fields.input['order']:
-            for p in TUNABLE_PARAMS:
-                df_results.loc[(fid, mode), f'{p}_mean'] = tracker.__getattribute__(p)[0, 0]
-                df_results.loc[(fid, mode), f'{p}_std'] = np.nan
 
     if os.path.exists(summary_csv):
         df_existing = pd.read_csv(summary_csv)
@@ -250,7 +232,7 @@ def insert_blank_rows(csv, bad_data_csv=None):
     df = pd.read_csv(csv)
     new_df = pd.DataFrame()
     fids = df['fid'].unique()
-    bad_df =  pd.DataFrame()
+    bad_df = pd.DataFrame()
 
     for i, fid in enumerate(fids):
         subset = df[df['fid'] == fid]
@@ -290,9 +272,10 @@ if __name__ == '__main__':
     # 'ALARC2_Smith6'
 
     project_ws_ = os.path.join(root, 'tutorials', project)
-    summary = os.path.join(project_ws_, 'results_comparison.csv')
+    summary = os.path.join(project_ws_, 'results_comparison_05MAR2025_crops_tight.csv')
+    update_dir = '/data/ssd2/swim/4_Flux_Network/results'
 
-    for mode_ in ['loose', 'tight', 'uncal']:
+    for mode_ in ['tight']:  # , 'loose', 'uncal'
 
         data_ = os.path.join(project_ws_, 'data')
 
@@ -302,13 +285,10 @@ if __name__ == '__main__':
         if mode_ == 'uncal':
             forecast_ = False
             calibration_dir = None
-            update_params = None
         else:
             ct = 3
             forecast_ = True
-            update_params = '/data/ssd2/swim/4_Flux_Network/results'
             calibration_dir = os.path.join(project_ws_, f'{mode_}_master')
-
 
         out_csv_dir = os.path.join(data_, f'model_output_{mode_}')
         if not os.path.exists(out_csv_dir):
@@ -320,7 +300,8 @@ if __name__ == '__main__':
         # open properties instead of SamplePlots object for speed
         properties_json = os.path.join(data_, 'landsat', 'calibration_dynamics.json')
         compare_results(config_file, project_ws_, out_csv_dir, summary_csv=summary, select=None,
-                        cal_dir=calibration_dir, mode=mode_, updates=update_params)
+                        cal_dir=calibration_dir, mode=mode_, updates=update_dir, overwrite=False,
+                        check_individual=True)
 
     insert_blank_rows(summary, bad_data_csv=summary.replace('.csv', '_bad.csv'))
 
