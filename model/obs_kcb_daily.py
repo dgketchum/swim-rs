5719dbab54513ae40cb657bae65b02ce0c2ef482
dgketchum <dgketchum@gmail.com>
2025-03-08 12:00:49 -0700

    initial commit sigmoid

--- model/obs_kcb_daily.py
+++ model/obs_kcb_daily.py
@@ -12,7 +12,11 @@ import numpy as np
 def kcb_daily(tracker, day_data):
     """"""
 
-    tracker.kc_bas = day_data.ndvi * tracker.ndvi_beta + tracker.ndvi_alpha
+    # linear
+    # tracker.kc_bas = day_data.ndvi * tracker.ndvi_beta + tracker.ndvi_alpha
+    # sigmoid
+    tracker.kc_bas = tracker. kcb_max / (1 + np.exp(-tracker.ndvi_k * (day_data.ndvi - tracker.ndvi_0)))
+
     tracker.kc_bas = np.minimum(tracker.kc_max, tracker.kc_bas)
 
     # Water has only 'kcb'
