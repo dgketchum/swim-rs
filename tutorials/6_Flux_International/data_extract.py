d93e00e4af21615f6e28c27f966981e1a627eacc
dgketchum <dgketchum@gmail.com>
2025-06-10 12:58:58 -0600

    working on ptjpl study refactor

--- tutorials/6_Flux_International/data_extract.py
+++ tutorials/6_Flux_International/data_extract.py
@@ -2,71 +2,35 @@ import os
 
 from data_extraction.ee.ee_utils import is_authorized
 from prep import get_flux_sites
-
-project = '6_Flux_International'
-
-root = '/data/ssd2/swim'
-data = os.path.join(root, project, 'data')
-project_ws = os.path.join(root, project)
-
-if not os.path.isdir(root):
-    root = os.path.join(os.path.expanduser('~'), 'PycharmProjects', 'swim-rs')
-    data = os.path.join(root, 'tutorials', project, 'data')
-    project_ws = os.path.join(root, 'tutorials', project)
-
-landsat = os.path.join(data, 'landsat')
-landsat_extracts = os.path.join(landsat, 'extracts')
-landsat_tables = os.path.join(landsat, 'tables')
-
-sentinel = os.path.join(data, 'sentinel')
-sentinel_extracts = os.path.join(sentinel, 'ee_extracts')
-sentinel_tables = os.path.join(sentinel, 'tables')
-
-era5_extracts = os.path.join(data, 'met_timeseries', 'extracts')
-
-modis_lulc = os.path.join(data, 'properties', f'{project}_landcover.csv')
-soils = os.path.join(data, 'properties', f'{project}_hwsd.csv')
-properties_json = os.path.join(data, 'properties', 'calibration_properties.json')
-
-# GCS - Earth Engine
-fields = 'projects/ee-dgketchum/assets/swim/6_Flux_International_landcover_150mBuf_13MAY2025'
-bucket_ = 'wudr'
-
-# ICOS 200m station buffer shapefile index
-FEATURE_ID = 'sid'
-
-# European crop sites
-shapefile_path = os.path.join(data, 'gis', '6_Flux_International_150mBuf.shp')
-sites = get_flux_sites(shapefile_path, index_col=FEATURE_ID)
+from swim.config import ProjectConfig
 
 
-def extract_era5land():
+def extract_era5land(conf):
     from data_extraction.ee.ee_era5 import sample_era5_land_variables_daily
     is_authorized()
 
     sample_era5_land_variables_daily(
-        feature_coll_asset_id=fields,
-        bucket=bucket_,
+        feature_coll_asset_id=conf.ee_fields_flux,
+        bucket=conf.ee_bucket,
         debug=False,
-        check_dir=era5_extracts,
+        check_dir=None,
         overwrite=False,
         start_yr=2015,
         end_yr=2025,
-        feature_id_col=FEATURE_ID
+        feature_id_col=conf.feature_id_col,
     )
 
 
-def extract_properties():
+def extract_properties(conf):
     is_authorized()
     from data_extraction.ee.ee_props import get_landcover, get_hwsd
 
-    index_col = 'sid'
-    get_landcover(fields, None, debug=False, selector=index_col, local_file=modis_lulc)
+    get_landcover(conf.ee_fields_flux, None, debug=False, selector=conf.feature_id_col, local_file=None)
 
-    get_hwsd(fields, None, debug=False, selector=index_col, local_file=soils)
+    get_hwsd(conf.ee_fields_flux, None, debug=False, selector=conf.feature_id_col, local_file=None)
 
 
-def extract_remote_sensing():
+def extract_remote_sensing(conf, sites):
     is_authorized()
     from data_extraction.ee.ndvi_export import sparse_sample_ndvi
 
@@ -74,20 +38,35 @@ def extract_remote_sensing():
     mask = 'no_mask'
 
     print('landsat', src, mask)
-    dst = os.path.join(landsat_extracts, src, mask)
-    sparse_sample_ndvi(shapefile_path, bucket=bucket_, debug=False, satellite='landsat',
-                       mask_type=mask, check_dir=dst, start_yr=2015, end_yr=2024, feature_id=FEATURE_ID,
+    dst = os.path.join(conf.landsat_ee_data_dir, src, mask)
+    sparse_sample_ndvi(conf.footprint_shapefile_shp, bucket=conf.ee_bucket, debug=False, satellite='landsat',
+                       mask_type=mask, check_dir=dst, start_yr=2015, end_yr=2024, feature_id=conf.feature_id_col,
                        select=sites)
 
     print('sentinel', src, mask)
-    dst = os.path.join(sentinel_extracts, src, mask)
-    sparse_sample_ndvi(shapefile_path, bucket=bucket_, debug=False, satellite='sentinel',
-                       mask_type=mask, check_dir=dst, start_yr=2017, end_yr=2024, feature_id=FEATURE_ID,
+    dst = os.path.join(conf.sentinel_ee_data_dir, src, mask)
+    sparse_sample_ndvi(conf.footprint_shapefile_shp, bucket=conf.ee_bucket, debug=False, satellite='sentinel',
+                       mask_type=mask, check_dir=dst, start_yr=2017, end_yr=2024, feature_id=conf.feature_id_col,
                        select=sites)
 
 
 if __name__ == '__main__':
-    extract_properties()
-    extract_era5land()
-    extract_remote_sensing()
+
+
+    project = '6_Flux_International'
+    western_only = False
+
+    home = os.path.expanduser('~')
+    config_file = os.path.join(home, 'PycharmProjects', 'swim-rs', 'tutorials', project, f'{project}.toml')
+
+    config = ProjectConfig()
+    config.read_config(config_file)
+
+    select_sites = get_flux_sites(config.station_metadata_csv, crop_only=False, western_only=western_only, header=1,
+                                  index_col=0)
+
+    extract_era5land(config)
+    extract_properties(config)
+    extract_remote_sensing(config, select_sites)
+
 # ========================= EOF ====================================================================
