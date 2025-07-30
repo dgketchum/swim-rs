74862e407fc2f71b71fbb8dc3d8770c7d6386f61
dgketchum <dgketchum@gmail.com>
2025-07-30 08:40:27 -0600

    limit zr_new to zr_max

--- model/grow_root.py
+++ model/grow_root.py
@@ -7,6 +7,7 @@ def grow_root(tracker, day_data):
     zr_prev = tracker.zr.copy()
 
     zr_new = (tracker.kc_bas - tracker.kc_min) / (tracker.kc_max - tracker.kc_min) * tracker.zr_max
+    zr_new = np.clip(zr_new, np.zeros_like(tracker.zr_max), tracker.zr_max)
 
     tracker.zr = np.where(tracker.perennial, zr_prev, zr_new)
     delta_zr = tracker.zr - zr_prev
@@ -17,7 +18,7 @@ def grow_root(tracker, day_data):
 
     tracker.depl_root = np.where(delta_zr > 0.0,
                                  tracker.depl_root + (delta_zr * tracker.aw) - (
-                                         tracker.daw3 * delta_zr / (tracker.zr_max - tracker.zr + 1e-6)),
+                                     tracker.daw3 * delta_zr / (tracker.zr_max - tracker.zr + 1e-6)),
                                  tracker.depl_root * (1 - (-1.0 * delta_zr / zr_prev)))
 
     rt_water = (tracker.aw * tracker.zr) - tracker.depl_root.copy()
@@ -30,7 +31,7 @@ def grow_root(tracker, day_data):
 
     if np.any(tracker.daw3 - tracker.taw3 > 0.0001):
         print(day_data.dt_string)
-        raise NotImplementedError(f'daw3/taw3 mass failure: zr_prev: {zr_prev[0, 0]:.2f}, zr_new {zr_new[0, 0]:.2f}')
+        raise NotImplementedError(f'daw3/taw3 mass failure: zr_prev: {zr_prev[0, 0]:.4f}, zr_new {zr_new[0, 0]:.4f}')
 
     delta_daw3 = tracker.daw3 - prev_daw3
     delta_rt_water = rt_water - prev_rt_water
